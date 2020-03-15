0c85a475c4e4389df9139b3a979357f5d96d9da8
ForeztNoize <61051092+ForeztNoize@users.noreply.github.com>
2020-03-15 19:16:06 +0100

    Converter for naming and giving the cars values made.

--- BongoGane/src/bongo/Main.java
+++ BongoGane/src/bongo/Main.java
@@ -1,9 +1,56 @@
 package bongo;
 
+import java.awt.BorderLayout;
 import java.util.ArrayList;
 
-public class Main {
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-	Deck deck = new Deck();
-	ArrayList<Cards> Deck = deck.getDeck();
+public class Main {
+	public static JFrame frame = new JFrame("BongoGame");
+	public static JPanel panel = new JPanel();
+	public static JLabel lblNewLabel = new JLabel("");
+	
+	public static void main(String[] args) {
+		panel.setBounds(0, 5, 571, 33);
+		frame.getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+	
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		lblNewLabel.setBounds(30, 9, 150, 14);
+		panel.add(lblNewLabel);	
+		Deck deck = new Deck();
+		ArrayList<Cards> Deck = deck.getDeck();
+		int y = 0;
+		int z = 0;
+		for (int i = 0; i < Deck.size(); i++) {
+			int color = Deck.get(z).getColor();
+			int value = Deck.get(z).getValue();
+			String colortransform = null;
+			switch(color) {
+			case 1:
+				colortransform = "Red";
+				break;
+			case 2:
+				colortransform = "Blue";
+				break;
+			case 3:
+				colortransform = "Green";
+				break;
+			case 4:
+				colortransform = "Yellow";
+				break;
+			default:
+				break;
+			}
+			JLabel Values[] = new JLabel[36];
+			Values[z]  = new JLabel(colortransform + value);
+			Values[z].setBounds(30, y, 150, 14);
+			y += 40;
+			panel.add(Values[z]);
+			z++;
+		}
+	}
 }
