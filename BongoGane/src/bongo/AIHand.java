9f7d72c39ce3adb817e5968897744e72220323bd
ForeztNoize <61051092+ForeztNoize@users.noreply.github.com>
2020-03-19 21:52:25 +0100

    Added UML chart and final changes.

--- BongoGane/src/bongo/AIHand.java
+++ BongoGane/src/bongo/AIHand.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class AIHand {
@@ -12,18 +13,16 @@ public class AIHand {
 	private static ArrayList<JButton> kno = new ArrayList<JButton>();
 	private static int aicards = 10;
 	private static JButton[] crds = new JButton[aicards];
+	private static int Score = 0;
 	
 	public static ArrayList<Cards> getP2deck() {
 		return p2deck;
 	}
 	
 	public AIHand(JPanel panel,int x, int y, int i) {
-		String value = Hand.getDongle().get(0).CardTotalValue();
-		JButton card = new JButton(value);
+		JButton card = new JButton("Bongo");
 		int id = i;
 		card.setBounds(x, y, 80, 100);
-		Color currentColor = Hand.getDongle().get(0).getColor();
-		card.setBackground(currentColor);
 		p2deck.add(Hand.getDongle().get(0));
 		Hand.getDongle().remove(0);
 		panel.add(card);
@@ -43,30 +42,52 @@ public class AIHand {
 		kno.set(0, computerChoice);
 		crds[id].setVisible(false);
 		panel.add(kno.get(0));
-		Analyzer(player, activeColor);
-		Analyzer(getP2deck().get(id), player.getColor());
+		
+		int playerdamage = Analyzer(player, activeColor);
+		int computerdamage = Analyzer(getP2deck().get(id), player.getColor());
+		int playernumber = Integer.parseInt(player.CardTotalValue());
+		int computernumber = Integer.parseInt(getP2deck().get(id).CardTotalValue());
+		int playerTotal = playerdamage + playernumber;
+		int computerTotal = computerdamage + computernumber;
+		Game(playerTotal, computerTotal);
 	}
 	
-	public static void Analyzer(Cards subject, Color activeColor) {
+	public static int Analyzer(Cards subject, Color activeColor) {
+		int damage = 1;
 		if(subject.getColor() == Color.RED) {
 			Fire playerFire = new Fire();
 			playerFire.Action();
-			playerFire.Force(activeColor);
+			damage = playerFire.Force(activeColor);
 		}
 		if(subject.getColor() == Color.GREEN) {
 			Grass playerGrass = new Grass();
 			playerGrass.Action();
-			playerGrass.Force(activeColor);
+			damage = playerGrass.Force(activeColor);
 		}
 		if(subject.getColor() == Color.BLUE) {
 			Water playerWater = new Water();
 			playerWater.Action();
-			playerWater.Force(activeColor);
+			damage = playerWater.Force(activeColor);
 		}
 		if(subject.getColor() == Color.YELLOW) {
 			Poison playerPoison = new Poison();
 			playerPoison.Action();
-			playerPoison.Force(activeColor);
+			damage = playerPoison.Force(activeColor);
+		}
+		return damage;
+	}
+	
+	public static void Game(int player, int computer){
+		if(player > computer) {
+			Score++;
+			JOptionPane.showMessageDialog(null, "You Win\n Current Score: " + Score);
+		}
+		if(player < computer) {
+			Score--;
+			JOptionPane.showMessageDialog(null, "You Lose\n Current Score: " + Score);
+		}
+		if(player == computer) {
+			JOptionPane.showMessageDialog(null, "Draw\n Current Score: " + Score);
 		}
 	}
 }
